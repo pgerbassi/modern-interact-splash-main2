76ab8b95e757a525cab46d7450e63fc8f54433b0
pcsgerbassi <pcsgerbassi@gmail.com>
2025-02-27 12:52:44 -0300

    period change

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -84,7 +84,7 @@ const Pricing = () => {
             <PricingCard
               type="Aproveite!"
               price={397}
-              period={isYearly ? "" : "por 1 ano."}
+              period={isYearly ? "" : "por 6 meses."}
               buttonText="Seja Membro!"
               variant="default"
             />
