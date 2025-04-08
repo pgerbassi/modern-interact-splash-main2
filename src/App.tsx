071686a6381aec7c71af79d648e992f6053dea08
Pablo <pcsgerbassi@gmail.com>
2025-04-08 12:29:09 -0300

    Fix: Routes

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,27 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Upsell from "./components/Upsell";
 import IndexMv from "./pages/indexMv";
+import routesData from '../vercel.json';
 
 const queryClient = new QueryClient();
+const routeComponents = { Index, Upsell, IndexMv };
 
 const App = () => (
+
   <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/upsell" element={<Upsell />} />
-          <Route path="/metodomv" element={<IndexMv />} />
-        </Routes>
-      </BrowserRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
+  <TooltipProvider>
+    <Toaster />
+    <Sonner />
+    <BrowserRouter>
+    <Routes>
+      {routesData.routes.map((route, index) => {
+        const Component = routeComponents[route.element];
+        return <Route key={index} path={route.path} element={<Component />} />;
+      })}
+    </Routes>
+    </BrowserRouter>
+  </TooltipProvider>
+</QueryClientProvider>
 );
 
 export default App;
